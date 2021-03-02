1d22d409c88b32729d638a2910e9f5d7a3c88af3
REevee0 <79875968+REevee0@users.noreply.github.com>
2021-03-02 23:25:00 +0300

    Update LCDLib.h

--- LCDLib.h
+++ LCDLib.h
@@ -5,28 +5,28 @@
 #include "stm32f2xx.h"
 #include "stdio.h"
 
-#define LCDD0Pin 3
-#define LCDD0Port GPIOA
-#define LCDD1Pin 4
-#define LCDD1Port GPIOA
-#define LCDD2Pin 5
-#define LCDD2Port GPIOA
-#define LCDD3Pin 6
-#define LCDD3Port GPIOA
-#define LCDD4Pin 7
-#define LCDD4Port GPIOA
-#define LCDD5Pin 4
-#define LCDD5Port GPIOC
-#define LCDD6Pin 5
-#define LCDD6Port GPIOC
-#define LCDD7Pin 0
-#define LCDD7Port GPIOB
-#define LCDEPin 11
-#define LCDEPort GPIOB
-#define LCDRWPin 10
-#define LCDRWPort GPIOB
-#define LCDRSPin 1
-#define LCDRSPort GPIOB
+#define LCDD0Pin D0_Pin
+#define LCDD0Port D0_GPIO_Port
+#define LCDD1Pin D1_Pin
+#define LCDD1Port D1_GPIO_Port
+#define LCDD2Pin D2_Pin
+#define LCDD2Port D2_GPIO_Port
+#define LCDD3Pin D3_Pin
+#define LCDD3Port D3_GPIO_Port
+#define LCDD4Pin D4_Pin
+#define LCDD4Port D4_GPIO_Port
+#define LCDD5Pin D5_Pin
+#define LCDD5Port D5_GPIO_Port
+#define LCDD6Pin D6_Pin
+#define LCDD6Port D6_GPIO_Port
+#define LCDD7Pin D7_Pin
+#define LCDD7Port D7_GPIO_Port
+#define LCDEPin E_Pin
+#define LCDEPort E_GPIO_Port
+#define LCDRWPin RW_Pin
+#define LCDRWPort RW_GPIO_Port
+#define LCDRSPin RS_Pin
+#define LCDRSPort RS_GPIO_Port
 #define delayBE 4000
 #define delayBD 8000
 
